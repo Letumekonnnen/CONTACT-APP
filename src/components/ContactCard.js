2fe538f5d2ecd2a3ad21c5111f809f0d593085b3
Letu Mekonnen <letinamekonnen6@gmail.com>
2024-01-17 22:59:13 +0300

    added

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,10 +1,13 @@
 import React from "react";
+import user from '../images/user.jpg';
 
 const ContactCard = (props) =>{
     const  {id, name, email} = props.Contacts;
 
     return(
         <div className = "item">
+            <img className="ui avatarimage image" src={user} alt="user" 
+              style={{ width: '50px', height: '50px' }} />
                 <div className ="content">
                     <div className ="header">{name}</div>
                     <div>{email}</div>
